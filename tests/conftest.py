ed627d05eb3a491006f35f2ffe17eaf3f71c7354
Yaroslav Gusev <yarigusev@gmail.com>
2025-03-08 21:46:24 +0300

    add web and mobile tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,27 +7,77 @@ from selene import browser
 from selenium import webdriver
 from utils import config_mobile
 from utils.config import base_url
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver import Chrome
+from common import attach
+from selenium.webdriver.chrome.options import Options
+
+DEFAULT_BROWSER_VERSION = '126.0'
+DEFAULT_WEB_ENVIRONMENT = 'local'
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default=DEFAULT_BROWSER_VERSION
+    )
+    parser.addoption(
+        '--web_env',
+        default=DEFAULT_WEB_ENVIRONMENT
+    )
 
 
 @pytest.fixture(scope='function')
-def browser_management():
+def browser_management(request):
+    browser_version = request.config.getoption('browser_version') or DEFAULT_BROWSER_VERSION
+    web_env = request.config.getoption('web_env') or DEFAULT_WEB_ENVIRONMENT
     driver_options = webdriver.ChromeOptions()
 
-    driver_options.page_load_strategy = 'eager'
-    driver_options.add_argument('--headless=new')
-    driver_options.add_argument('--no-sandbox')
+    if web_env == 'local':
 
-    # Раскомментируйте, если требуется запуск в полноэкранном режиме.
-    # driver_options.add_argument('--window-size=1920,1024')
+        driver_options.page_load_strategy = 'eager'
+        driver_options.add_argument('--headless=new')
+        driver_options.add_argument('--no-sandbox')
 
-    service = Service(ChromeDriverManager().install())
-    browser.config.driver = Chrome(service=service, options=driver_options)
+        # Раскомментируйте, если требуется запуск в полноэкранном режиме.
+        # driver_options.add_argument('--window-size=1920,1024')
+
+    else:
+        options = Options()
+        selenoid_capabilities = {
+            "browserName": "chrome",
+            "browserVersion": browser_version,
+            "selenoid:options": {
+                "enableVNC": True,
+                "enableVideo": True
+            }
+        }
+        options.capabilities.update(selenoid_capabilities)
+
+        selenoid_login = os.getenv("SELENOID_LOGIN")
+        selenoid_pass = os.getenv("SELENOID_PASS")
+        selenoid_url = os.getenv("SELENOID_URL")
+
+        driver = webdriver.Remote(
+            command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+            options=options,
+            keep_alive=True
+        )
+
+        browser.config.driver = driver
+
+        driver_options.page_load_strategy = 'eager'
+        driver_options.add_argument('--window-size=1920,1024')
 
     browser.config.driver_options = driver_options
     browser.config.base_url = base_url
+
+    yield
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
+    browser.quit()
 
 
 @pytest.fixture(scope='function')
